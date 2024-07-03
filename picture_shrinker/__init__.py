b577144e2bb43ed3a9e22ab933f859067103ea6f
NovaHFly <mugiviska@gmail.com>
2024-07-03 14:02:19 +0300

    feat: Add mode options to cli, fully working script for single picture

--- picture_shrinker/__init__.py
+++ picture_shrinker/__init__.py
@@ -10,15 +10,32 @@ from picture_shrinker.shrinker import Mode, process_picture
     'path',
     type=cloup.Path(exists=True, readable=True, path_type=Path),
 )
-def main_cli(path: Path) -> None:
+@cloup.option_group(
+    'mode',
+    cloup.option('--greater', metavar='SIZE', type=cloup.INT),
+    cloup.option('--lesser', metavar='SIZE', type=cloup.INT),
+    cloup.option('--multiplier', metavar='MULTIPLIER', type=cloup.FLOAT),
+    constraint=cloup.constraints.RequireExactly(1),
+)
+def main_cli(path: Path, greater: int, lesser: int, multiplier: float) -> None:
     """Shrink image(s) from path."""
     if path.is_dir():
         print("Can't process dirs for now")
         return
 
-    mode = Mode.MULTIPLIER
-    desired_size = 1800
-    multiplier = 0.5
+    size: float
+
+    if greater is not None:
+        mode = Mode.GREATER_SIZE
+        size = greater
+
+    if lesser is not None:
+        mode = Mode.LESSER_SIZE
+        size = lesser
+
+    if multiplier is not None:
+        mode = Mode.MULTIPLIER
+        size = multiplier
 
     print(path)
-    process_picture(path, mode, multiplier)
+    process_picture(path, mode, size)
